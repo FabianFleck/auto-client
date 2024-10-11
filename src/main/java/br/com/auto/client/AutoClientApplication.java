92721140c99462fa77fa687c50b4ccabd4c334c5
Fabian Fleck <faabianfleeck@gmail.com>
2024-10-11 00:43:22 -0300

    first commit

---
+++ src/main/java/br/com/auto/client/AutoClientApplication.java
@@ -0,0 +1,14 @@
+package br.com.auto.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class AutoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/auto/client/AutoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.auto.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
